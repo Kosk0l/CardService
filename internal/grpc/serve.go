e41bb3cffbfe7769ecf61d215a90eba3bd4d23ea
Kosk0l <kosk0l@yandex.ru>
2025-11-10 21:36:13 +0300

    grpc Server Handlers

---
+++ internal/grpc/serve.go
@@ -0,0 +1,37 @@
+package grpc
+
+import (
+	"CardService/proto/grpcProto"
+	"context"
+)
+
+type Server struct {
+
+}
+
+//===================================================================================================================//
+
+func (s *Server) GetCardRequest(ctx context.Context, req *grpcProto.GetCardRequest) (*grpcProto.CardResponse, error) {
+
+	return nil ,nil
+}
+
+func (s *Server) CreateCardRequest(ctx context.Context, req *grpcProto.CreateCardRequest) (*grpcProto.CardResponse, error) {
+
+	return nil, nil	
+}
+
+func (s *Server) UpdateCardRequest(ctx context.Context, req *grpcProto.UpdateCardRequest) (*grpcProto.CardResponse, error) {
+
+	return nil, nil
+}
+
+func (s *Server) DeleteCardRequest(ctx context.Context, req *grpcProto.CreateCardRequest) (*grpcProto.DeleteCardResponse, error) {
+
+	return nil, nil
+}
+
+func (s *Server) ListCardRequest(ctx context.Context, req *grpcProto.ListCardRequest) (*grpcProto.CardResponse, error) {
+
+	return nil, nil
+}
