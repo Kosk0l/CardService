4eb15da55a18dbf582e576aa9757eb8092f6834f
Kosk0l <kosk0l@yandex.ru>
2025-11-12 00:24:17 +0300

    gRPC_First_Server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	pb "CardService/proto/grpcProto"
 	"log"
 	"net"
-
 	"google.golang.org/grpc"
+	grpcServer "CardService/internal/grpc"
 )
 
 func main() {
 	// Настройка слушателя
-	lis, err := net.Listen("tcp", "44046")
+	lis, err := net.Listen("tcp", ":44045")
 	if err != nil {
 		log.Fatalf("Error listen server: %v", err)
 	}
 
 	// Иннициализация сервера
 	s := grpc.NewServer()
+	pb.RegisterCardServiceServer(s, &grpcServer.Server{})
 
+	log.Println("Server working")
 	// Настройка серве слушателя
 	if err := s.Serve(lis); err != nil { 
 		log.Fatalf("failed to serve: %v", err)
--- internal/grpc/serve.go
+++ internal/grpc/serve.go
@@ -11,27 +11,27 @@ type Server struct {
 
 //===================================================================================================================//
 
-func (s *Server) GetCardRequest(ctx context.Context, req *grpcProto.GetCardRequest) (*grpcProto.CardResponse, error) {
+func (s *Server) GetCard(ctx context.Context, req *grpcProto.GetCardRequest) (*grpcProto.CardResponse, error) {
 
 	return nil ,nil
 }
 
-func (s *Server) CreateCardRequest(ctx context.Context, req *grpcProto.CreateCardRequest) (*grpcProto.CardResponse, error) {
+func (s *Server) CreateCard(ctx context.Context, req *grpcProto.CreateCardRequest) (*grpcProto.CardResponse, error) {
 
 	return nil, nil	
 }
 
-func (s *Server) UpdateCardRequest(ctx context.Context, req *grpcProto.UpdateCardRequest) (*grpcProto.CardResponse, error) {
+func (s *Server) UpdateCard(ctx context.Context, req *grpcProto.UpdateCardRequest) (*grpcProto.CardResponse, error) {
 
 	return nil, nil
 }
 
-func (s *Server) DeleteCardRequest(ctx context.Context, req *grpcProto.CreateCardRequest) (*grpcProto.DeleteCardResponse, error) {
+func (s *Server) DeleteCard(ctx context.Context, req *grpcProto.DeleteCardRequest) (*grpcProto.DeleteCardResponse, error) {
 
 	return nil, nil
 }
 
-func (s *Server) ListCardRequest(ctx context.Context, req *grpcProto.ListCardRequest) (*grpcProto.CardResponse, error) {
+func (s *Server) ListCard(req *grpcProto.ListCardRequest, stream grpcProto.CardService_ListCardServer) error {
 
-	return nil, nil
+	return nil
 }
