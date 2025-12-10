318de8b9204492330045a1f3c4ff37fda51ab105
Kosk0l <kosk0l@yandex.ru>
2025-12-10 15:17:03 +0300

    Application

---
+++ internal/app/app.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"CardService/config"
+	"CardService/internal/grpchandler"
+	"CardService/internal/server"
+	"CardService/internal/services"
+	"CardService/internal/storage"
+	"context"
+)
+
+type App struct {
+	grpc *server.GRPCServer
+}
+
+// Связка компанентов
+func NewApp(ctx context.Context, cfg config.Config) (*App, error) {
+	// Объект storage
+	pool, err := storage.NewPostgres(ctx, cfg.DataBaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// Объект бизнес-логики
+	repo := pool
+	services := services.NewService(repo)
+
+	// Объект gRPC handler
+	handler := grpchandler.NewServer(services)
+
+	// Объект сервера
+	grpcServer := server.NewGrpcServer(cfg.GRPCPort, handler)
+
+	return &App{
+		grpc: grpcServer,
+	}, nil
+}
+
+func (s *App) Run() (error) {
+	return s.grpc.Start()
+}
+
+func (s *App) Stop() () {
+	s.grpc.Stop()
+}
